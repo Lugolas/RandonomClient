608ce07c1497b25cb0a650e088103ee13e241953
Thanaen <Clem0970@gmail.com>
2019-02-23 13:47:20 +0100

    Changing the way names are fetched
    It doesn't work yet, but I'm trying to load and display names on page load

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,119 +1,144 @@
-import React, { Component } from 'react';
-import { Button, Row, Table, Tag, } from "antd";
-import moment from 'moment';
+import React, { Component } from "react";
+import { Button, Row, Table, Tag } from "antd";
+import moment from "moment";
 import { capitalizeFirstLetter } from "./../utils/utils";
-require('moment/locale/fr');
+import { fetchNames } from "./../utils/utils";
+require("moment/locale/fr");
 
-const columns = [{
-  title: 'Nom',
-  dataIndex: 'value',
-  key: 'value',
-}, {
-  title: 'Categorie',
-  dataIndex: 'type',
-  key: 'type',
-  render: categorie => {
-    let text;
-    if (categorie) {
-      text = "Nom de famille"
-    } else { 
-      text = "Prénom"
-    }
-    return text;
+const columns = [
+  {
+    title: "Nom",
+    dataIndex: "value",
+    key: "value"
   },
-}, {
-  title: 'Préfixable',
-  dataIndex: 'prefixable',
-  key: 'prefixable',
-  render: prefixable => (
-    <span>
-      <Tag color={prefixable ? 'geekblue' : 'green'} key={prefixable + '' + Math.floor((Math.random() * 1000) + 1)}>{prefixable ? 'NON' : 'OUI'}</Tag>
-    </span>
-  ),
-}, {
-  title: 'Suffixable',
-  dataIndex: 'suffixable',
-  key: 'suffixable',
-  render: suffixable => (
-    <span>
-      <Tag color={suffixable ? 'geekblue' : 'green'} key={suffixable + '' + Math.floor((Math.random() * 1000) + 1)}>{suffixable ? 'NON' : 'OUI'}</Tag>
-    </span>
-  ),
-}, {
-  title: 'A été inventé le',
-  dataIndex: 'createdAt',
-  key: 'createdAt',
-  render: date => {
-    moment.locale(); 
-    return capitalizeFirstLetter(moment(date).fromNow())
+  {
+    title: "Categorie",
+    dataIndex: "type",
+    key: "type",
+    render: categorie => {
+      let text;
+      if (categorie) {
+        text = "Nom de famille";
+      } else {
+        text = "Prénom";
+      }
+      return text;
+    }
+  },
+  {
+    title: "Préfixable",
+    dataIndex: "prefixable",
+    key: "prefixable",
+    render: prefixable => (
+      <span>
+        <Tag
+          color={prefixable ? "geekblue" : "green"}
+          key={prefixable + "" + Math.floor(Math.random() * 1000 + 1)}
+        >
+          {prefixable ? "NON" : "OUI"}
+        </Tag>
+      </span>
+    )
+  },
+  {
+    title: "Suffixable",
+    dataIndex: "suffixable",
+    key: "suffixable",
+    render: suffixable => (
+      <span>
+        <Tag
+          color={suffixable ? "geekblue" : "green"}
+          key={suffixable + "" + Math.floor(Math.random() * 1000 + 1)}
+        >
+          {suffixable ? "NON" : "OUI"}
+        </Tag>
+      </span>
+    )
+  },
+  {
+    title: "A été inventé le",
+    dataIndex: "createdAt",
+    key: "createdAt",
+    render: date => {
+      moment.locale();
+      return capitalizeFirstLetter(moment(date).fromNow());
+    }
   }
-}];
+];
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       gameHasBegun: false,
-      randonoms: [],
+      randonoms: []
     };
   }
 
   onClickBefore = () => {
-    fetch('https://randonomserver.azurewebsites.net/api/names')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        this.setState({
-          gameHasBegun: true,
-          randonoms: data,
-        })
-      });
-  }
+    var result = fetchNames();
+
+    this.setState({
+      gameHasBegun: true,
+      randonoms: result
+    })
+  };
+
+  componentDidMount = () => {
+    var result = fetchNames();
+
+    this.setState({
+      gameHasBegun: true,
+      randonoms: result
+    });
+  };
 
   onClickAfter = () => {
     this.setState({
-      gameHasBegun: false,
-    })
-  }
+      gameHasBegun: false
+    });
+  };
 
   renderBefore = () => {
     return (
       <div>
-        <Row>
-          Let the game begin
-        </Row>
+        <Row>Let the game begin</Row>
 
-        <Row>
-          <Button type="primary" onClick={this.onClickBefore}>Heyoo Let's gooo</Button>
-        </Row>
+        {/* <Row>
+          <Button type="primary" onClick={this.onClickBefore}>
+            Heyoo Let's gooo
+          </Button>
+        </Row> */}
       </div>
-    )
+    );
   };
-
-
 
   renderAfter = () => {
     return (
       <div>
-        <Row>
-          Yes that was so good
-        </Row>
+        {/* <Row>Yes that was so good</Row> */}
 
-        <Row>
-          <Button type="primary" onClick={this.onClickAfter}>Calm Down Now</Button>
-        </Row>
+        {/* <Row>
+          <Button type="primary" onClick={this.onClickAfter}>
+            Calm Down Now
+          </Button>
+        </Row> */}
 
-        <Table style={{margin: 100}} dataSource={this.state.randonoms} columns={columns}/>
+        <Table
+          style={{ margin: 100 }}
+          dataSource={this.state.randonoms}
+          columns={columns}
+        />
       </div>
-    )
+    );
   };
 
   render() {
     return (
       <div>
-        {
-          this.state.gameHasBegun === true ? this.renderAfter() : this.renderBefore() 
-        }
+        {this.state.gameHasBegun === true
+          ? this.renderAfter()
+          : this.renderBefore()}
       </div>
     );
   }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,18 @@
 export function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function fetchNames(){
+    fetch('https://randonomserver.azurewebsites.net/api/names')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+
+      return data;
+
+    //   this.setState({
+    //     gameHasBegun: true,
+    //     randonoms: data,
+    //   })
+    });
+}
