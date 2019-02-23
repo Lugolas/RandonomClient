6b3623bd7b4bbfa84016244bbaa8c5beb269423e
Thanaen <Clem0970@outlook.fr>
2019-02-23 23:19:18 +0100

    Small edits (Menu + preparing new components)
    Played with react-router / links
    Added Names.js & Extensions.js, but they're still empty

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import { Button, Layout, Row, Menu, Breadcrumb } from "antd";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import "./App.css";
 
@@ -15,23 +15,6 @@ class App extends Component {
 
   render() {
     return (
-      // //Maintenant
-      // <Router>
-      //   <div className="App">
-      //     <Header className="App-header">
-      //       <img src={logo} className="App-logo" alt="logo" />
-      //       <p>
-      //         Le processus <code>grulo.work</code> est en cours.
-      //       </p>
-      //     </Header>
-      //     <Content>
-      //       <Route exact path="/" component={Home} />
-      //     </Content>
-      //     <Footer className="footer">Inspiré de faits réels</Footer>
-      //   </div>
-      // </Router>
-
-      //Un beau layout Ant design tout prêt
       <Router>
         <Layout className="layout">
           <Header>
@@ -42,18 +25,18 @@ class App extends Component {
               defaultSelectedKeys={["2"]}
               style={{ lineHeight: "64px" }}
             >
-              <Menu.Item key="1">nav 1</Menu.Item>
-              <Menu.Item key="2">nav 2</Menu.Item>
-              <Menu.Item key="3">nav 3</Menu.Item>
+              <Menu.Item key="1"><Link to="/"></Link>Accueil</Menu.Item>
+              <Menu.Item key="2">Créer noms</Menu.Item>
+              <Menu.Item key="3">Créer extensions</Menu.Item>
             </Menu>
           </Header>
           <Content style={{ padding: "0 50px" }}>
-            <Breadcrumb style={{ margin: "16px 0" }}>
+            {/* <Breadcrumb style={{ margin: "16px 0" }}>
               <Breadcrumb.Item>Home</Breadcrumb.Item>
               <Breadcrumb.Item>List</Breadcrumb.Item>
               <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
-            <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
+            </Breadcrumb> */}
+            <div style={{ background: "#fff", padding: 24, minHeight: 280, marginTop: 16 }}>
               <Route exact path="/" component={Home} />
             </div>
           </Content>
