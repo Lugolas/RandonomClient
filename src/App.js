6c0797fe1bcf5f3095f74effead3c9792af4eeb5
Thanaen <Clem0970@gmail.com>
2019-02-23 13:29:54 +0100

    Added a new layout

--- src/App.js
+++ src/App.js
@@ -1,36 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Button, Layout, Row } from "antd";
-import { BrowserRouter as Router, Route, } from "react-router-dom"
-import Home from './components/Home'
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { Button, Layout, Row, Menu, Breadcrumb } from "antd";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import "./App.css";
 
-const { Header, Footer, Sider, Content, } = Layout;
+const { Header, Footer, Sider, Content } = Layout;
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
     return (
+      // //Maintenant
+      // <Router>
+      //   <div className="App">
+      //     <Header className="App-header">
+      //       <img src={logo} className="App-logo" alt="logo" />
+      //       <p>
+      //         Le processus <code>grulo.work</code> est en cours.
+      //       </p>
+      //     </Header>
+      //     <Content>
+      //       <Route exact path="/" component={Home} />
+      //     </Content>
+      //     <Footer className="footer">Inspiré de faits réels</Footer>
+      //   </div>
+      // </Router>
+
+      //Un beau layout Ant design tout prêt
       <Router>
-        <div className="App">
-          <Header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Le processus <code>grulo.work</code> est en cours.
-            </p>
+        <Layout className="layout">
+          <Header>
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={["2"]}
+              style={{ lineHeight: "64px" }}
+            >
+              <Menu.Item key="1">nav 1</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
           </Header>
-          <Content>
-            <Route exact path="/" component={Home} />
-            {/* <Route path="/about" component={About} />
-            <Route path="/topics" component={Topics} />  */}
+          <Content style={{ padding: "0 50px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
+              <Route exact path="/" component={Home} />
+            </div>
           </Content>
-          <Footer className="footer">Inspiré de faits réels</Footer>
-        </div>
+          <Footer style={{ textAlign: "center" }}>
+            Randonom ©2018 Created by Grulos Corp
+          </Footer>
+        </Layout>
       </Router>
     );
   }
